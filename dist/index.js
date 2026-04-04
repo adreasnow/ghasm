8dc5dd489608dc78d610716643e638e43c862def
adreasnow <adrea.snow@gmail.com>
2026-04-05 08:36:36 +1000

    Filter process.env to GITHUB_* and PATH for Go env

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,11 @@ const wasmBytes = await readFile(new URL("./main.wasm", import.meta.url));
 await import("./wasm_exec.js");
 
 const go = new Go();
-go.env = { ...process.env };
+go.env = Object.fromEntries(
+  Object.entries(process.env).filter(
+    ([k]) => k.startsWith("GITHUB_") || k === "PATH",
+  ),
+);
 WebAssembly.instantiate(wasmBytes, go.importObject).then((result) => {
   go.run(result.instance);
 });
