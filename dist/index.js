647a77f8f55902520f9af42698945134234162d2
adreasnow <adrea.snow@gmail.com>
2026-04-05 08:33:02 +1000

    Use readFile for WASM instantiation

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,11 @@
+import { readFile } from "node:fs/promises";
+
+const wasmBytes = await readFile(new URL("./main.wasm", import.meta.url));
+
 await import("./wasm_exec.js");
 
 const go = new Go();
 go.env = { ...process.env };
-WebAssembly.instantiateStreaming(fetch("./main.wasm"), go.importObject).then(
-  (result) => {
-    go.run(result.instance);
-  },
-);
+WebAssembly.instantiateStreaming(wasmBytes, go.importObject).then((result) => {
+  go.run(result.instance);
+});
