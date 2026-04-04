22d3ee975ce36db9fecf689fe597981fd84c7263
adreasnow <adrea.snow@gmail.com>
2026-04-05 08:30:31 +1000

    ./

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@ await import("./wasm_exec.js");
 
 const go = new Go();
 go.env = { ...process.env };
-WebAssembly.instantiateStreaming(fetch("main.wasm"), go.importObject).then(
+WebAssembly.instantiateStreaming(fetch("./main.wasm"), go.importObject).then(
   (result) => {
     go.run(result.instance);
   },
