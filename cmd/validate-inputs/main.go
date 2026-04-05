eee4cb83fec0cd4c3370eb83327b987b36ec8a8f
adreasnow <adrea.snow@gmail.com>
2026-04-06 09:28:18 +1000

    Remove unused os import and simplify error handling
    Use githubactions.Fatalf instead of Debugf with manual exit.

--- cmd/validate-inputs/main.go
+++ cmd/validate-inputs/main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"os"
-
 	"github.com/adreasnow/ghasm/internal/app/validateinputs"
 	"github.com/sethvargo/go-githubactions"
 )
 
 func main() {
 	if err := validateinputs.ValidateInputs(); err != nil {
-		githubactions.Debugf("error: %v", err)
-		os.Exit(1)
+		githubactions.Fatalf("%v", err)
 	}
 }
